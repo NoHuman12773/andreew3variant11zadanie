97e09e0c6f3a33e3ff5f3c6f24e0c4da977c58ea
NoHuman12773 <91951660+NoHuman12773@users.noreply.github.com>
2023-12-12 21:27:23 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,18 +1,22 @@
-def is_date(day, month, year):
-    return day == month
-def increment_month(day, month, year):
-    if month == 12:
-        month = 1
-        year += 1
-    else:
-        month += 1
-    return day, month, year
+class Date:
+    def is_date(day, month, year):
+        return day == month
+
+class isDate(Date):
+    def increment_month(day, month, year):
+        if month == 12:
+            month = 1
+            year += 1
+        else:
+            month += 1
+        return day, month, year
+
 day = 10
 month = 10
 year = 2023
 
-if is_date(day, month, year):
-    print("Nomer mesyaca i den sovpadaet")
+if Date.is_date(day, month, year):
+    print("Номер месяца и день совпадает")
 
-next_month_day, next_month, next_year = increment_month(day, month, year)
-print(f"Sled mesyac: {next_month}.{next_year}")
+next_month_day, next_month, next_year = isDate.increment_month(day, month, year)
+print(f"Следующий месяц: {next_month}.{next_year}")
